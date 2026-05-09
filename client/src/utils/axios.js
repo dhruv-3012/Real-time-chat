84a493b09a1207e7f8cf8277afbb8c8542f023bb
dhruv-3012 <dhruv.chib@gmail.com>
2026-05-09 16:32:11 +0530

    fix: hardcode API URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://real-time-chat-m227.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
